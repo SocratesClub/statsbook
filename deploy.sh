0a1ef624412e6e9f10ea9ab54520a1781be9dd3b
chengjun <wangchj04@gmail.com>
2021-11-17 17:11:53 +0800

    this is a message

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+# ssh-add ~/.ssh/id_rsa
+# git remote set-url origin git@github.com:socratesacademy/statsbook.git
+# git pull origin main
+# git add .
+# git commit -m 'this is a message'
+# git push origin main
+# open atom master branch
+jupyter-book build ../mybook/
+# Publish your book's HTML manually to GitHub pages
+# publish the _site folder of the main branch to the gh-pages branch
+ghp-import -n -p -f _build/html
